acd93102eda4f84ba902d4563a5d59f0a700c34d
daniel20773 <33464402+daniel20773@users.noreply.github.com>
2017-11-07 21:27:48 +0000

    New new new Cookie game hope this works

--- JSGamepage.js
+++ JSGamepage.js
@@ -1,53 +1,126 @@
-﻿function zoom(a) {
-    var x = document.getElementsByTagName("img")[0];
-    var cookies = document.getElementsByClassName("cookies")[0];
-    var multiplier = document.getElementsByTagName("span")[0];
-    if (a == 1) {
-        x.style.width = "65vw";
-        x.style.height = "65vw";
-        cookies.style.fontSize = "45px";
-        cookies.innerText = cookies.innerText.slice(0, -9) * 1 + multiplier.innerText * 1 + " Cookies!";
-        createNumbers("+" + multiplier.innerText);
-    } else {
-        x.style.width = "60vw";
-        x.style.height = "60vw";
-        cookies.style.fontSize = "40px";
-    }
-}
+﻿window.onload = function () {
 
-function createNumbers(num) {
-    var div = document.createElement("div");
-    div.innerText = num;
-    div.style.fontSize = "25px";
-    div.style.color = "white";
-    div.style.fontWeight = "bold";
-    div.style.position = "fixed";
-    div.style.top = Math.random() * 66 + 14 + "vh";
-    div.style.left = Math.random() * 90 + "vw";
-    div.style.opacity = 1;
-    document.getElementById("content").appendChild(div);
-    var hatsyrei = setInterval(function () {
-        if (div.style.opacity == 0.1) {
-            clearInterval(hatsyrei);
-            document.getElementById("content").removeChild(div);
-        }
-        div.style.opacity -= 0.1
-    }, 200);
-}
+    
+    alert("Thanks For Playing");
+    alert("Made By Daniel Strama-Rolfe");
 
-function upgrade() {
-    var cookies = document.getElementsByClassName("cookies")[0];
-    var multiplier = document.getElementsByTagName("span")[0];
-    var cost = document.getElementsByTagName("button")[0];
-    if (cookies.innerText.slice(0, -9) * 1 >= multiplier.innerText * 25) {
-        cookies.innerText = cookies.innerText.slice(0, -9) - multiplier.innerText * 25 + " Cookies!";
-        multiplier.innerText *= 2;
-        cost.innerHTML = "Upgrade<br>(" + multiplier.innerText * 25 + ")";
-        cost.style.color = "black";
-        if (multiplier.innerText == "4") {
-            alert("If you do like the game, I really appreciate it if you could like or upvote this code. I also accept suggestions (or compliments) in the comments section. Thank you for trying out my code random stranger!")
-        }
-    } else {
-        cost.style.color = "red"
+    /////////////////////////////////////////////
+    var myCookies = 0;
+    var clickPower = 1;
+    var price1 = 100;
+    var price2 = 20;
+    var price3 = 35;
+    var price4 = 200;
+    var cps = 0;
+    var buy1 = document.getElementById("buy1");
+    var cookie = document.getElementById("c");
+    var text = document.getElementById("text2");
+    var textPrice = document.getElementById("price1");
+    var buy2 = document.getElementById("buy2");
+    var textPrice2 = document.getElementById("price2");
+    var buy3 = document.getElementById("buy3");
+    var textPrice3 = document.getElementById("price3");
+    var buy4 = document.getElementById("buy4");
+    var textPrice4 = document.getElementById("price4");
+    var cpower = document.getElementById("clickPower");
+    var tcps = document.getElementById("tcps");
+    /////////////////////////////////////////////
+
+    //Cookie
+    cookie.onclick = function () {
+        myCookies += clickPower;
+        text.innerHTML = myCookies;
     }
-} 
+    //Cookie End
+
+    //Generate Cookie Automatically
+    function generate() {
+        myCookies++;
+        text.innerHTML = myCookies;
+    }
+    //Generate Cookie Automatically End
+
+    //Click Power
+    buy1.onclick = function () {
+
+        if (myCookies >= price1) {
+
+            myCookies -= price1;
+            text.innerHTML = myCookies;
+            buy1.innerHTML = "Upgrade";
+            buy1.style.backgroundColor = "Green";
+            clickPower *= 2;
+            price1 *= 3;
+            cpower.innerHTML = clickPower;
+            textPrice.innerHTML = price1;
+
+        }
+
+        else {
+
+            alert("Not Enough Cookies!");
+
+        }
+    }
+    //Click Power End
+
+    //Auto Click
+    buy2.onclick = function () {
+
+        if (myCookies >= price2) {
+
+            myCookies -= price2;
+            setInterval(generate, 1000);
+            cps++;
+            tcps.innerHTML = cps;
+            buy2.innerHTML = "Upgrade";
+            buy2.style.backgroundColor = "Green";
+            price2 *= 1.5;
+            textPrice2.innerHTML = price2;
+        }
+        else {
+            alert("Not Enough Cookies!");
+        }
+    }
+    //Auto Click End
+
+    //Grandma
+    buy3.onclick = function () {
+
+        if (myCookies >= price3) {
+
+            myCookies -= price3;
+            setInterval(generate, 500);
+            cps += 2;
+            tcps.innerHTML = cps;
+            buy3.innerHTML = "Upgrade";
+            buy3.style.backgroundColor = "Green";
+            price3 *= 1.5;
+            textPrice3.innerHTML = price3;
+        }
+        else {
+            alert("Not Enough Cookies!");
+        }
+    }
+    //Grandma End
+
+    //Cookie Farm
+    buy4.onclick = function () {
+
+        if (myCookies >= price4) {
+
+            myCookies -= price4;
+            setInterval(generate, 200);
+            cps += 5;
+            tcps.innerHTML = cps;
+            buy4.innerHTML = "Upgrade";
+            buy4.style.backgroundColor = "Green";
+            price4 *= 1.5;
+            textPrice4.innerHTML = price4;
+        }
+        else {
+            alert("Not Enough Cookies!");
+        }
+    }
+    //Cookie Farm End
+}
